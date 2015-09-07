1dc8da8ca1070b16f41fd692eaa65118e4655bb9
Jonathon Karu <jonakar@greeny.cs.tlu.ee>
2015-09-07 13:46:43 +0300

    added logic

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,24 @@
 <?php
 
-$first_name = "Jonathan";
+	$first_name = "Jonathan";
+	$last_name = "Karu";
 
-//tryki
-echo $first_name;
+	//tryki
+	echo $first_name." ".$last_name;
+
+?>
+
+<br>
+
+<?php
+
+	$age = 19;
+	
+	// if( loogikatehe ){true} else {false}
+	if($age <= 18){
+		echo "alaealine";
+	} else {
+		echo "t2isealine";
+	}
 
 ?>
